79f6fbf618fe8355508f3a36e926fb35fc2867f8
S8Vega <bsebastianvg18@gmail.com>
2020-10-10 16:39:24 -0500

    commit inicial

---
+++ src/main/java/com/ufpssgd/UfpsSgdApplication.java
@@ -0,0 +1,13 @@
+package com.ufpssgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfpsSgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfpsSgdApplication.class, args);
+	}
+
+}
