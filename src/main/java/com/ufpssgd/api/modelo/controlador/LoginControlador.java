8cf99e12669d4f4d5b6b1f6de7d5eb08ce7f045c
S8Vega <bsebastianvg18@gmail.com>
2020-12-13 11:32:31 -0500

    conectando vista de login

--- src/main/java/com/ufpssgd/api/modelo/controlador/LoginControlador.java
+++ src/main/java/com/ufpssgd/api/modelo/controlador/LoginControlador.java
@@ -1,19 +1,14 @@
 package com.ufpssgd.api.modelo.controlador;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.ufpssgd.api.modelo.servicio.implementacion.UsuarioServicioImpl;
 
 @Controller
 public class LoginControlador {
 
-	@Autowired
-	private UsuarioServicioImpl usuarioServicio;
-
 	@GetMapping
-	public String login() {
-		return "login";
+	public String login(Map<String, Object> model) {
+		return "index";
 	}
 }
---
+++ src/main/resources/static/js/administrador/administrador.js
@@ -0,0 +1,73 @@
+function loadInfo() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/informacion.html", true);
+    xhttp.send();
+  }
+
+  function loadDocuments() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/documentos.html", true);
+    xhttp.send();
+  }
+
+  function loadSolicitud() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/solicitudes.html", true);
+    xhttp.send();
+  }
+  
+  function loadNotificaciones() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/notificaciones.html", true);
+    xhttp.send();
+  }
+  function loadAggDocuments() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/subirDocumento.html", true);
+    xhttp.send();
+  }
+  function loadPanelBuzon() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/panelBuzon.html", true);
+    xhttp.send();
+  }
+  function loadRUser() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("contenedor").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "../../vista/administrador/rUsuario.html", true);
+    xhttp.send();
+  }
