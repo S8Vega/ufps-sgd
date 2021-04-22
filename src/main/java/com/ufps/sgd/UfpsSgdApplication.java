5eacaa1852b15dd97070aa6c3f8c00668b071096
S8Vega <bsebastianvg18@gmail.com>
2021-04-22 09:31:27 -0500

    agregando subida / descarga de archivos

--- src/main/java/com/ufps/sgd/UfpsSgdApplication.java
+++ src/main/java/com/ufps/sgd/UfpsSgdApplication.java
@@ -1,9 +1,14 @@
 package com.ufps.sgd;
 
+import com.ufps.sgd.domain.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class UfpsSgdApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ufps/sgd/domain/property/FileStorageProperties.java
@@ -0,0 +1,16 @@
+package com.ufps.sgd.domain.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+}
