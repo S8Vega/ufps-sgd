286abf8751561407ab8dd0171ced7d58e5b8e9c9
S8Vega <bsebastianvg18@gmail.com>
2021-04-23 21:35:16 -0500

    asociando archivos a los documentos que se cargan y descargan

--- src/main/java/com/ufps/sgd/domain/service/DocumentoService.java
+++ src/main/java/com/ufps/sgd/domain/service/DocumentoService.java
@@ -1,18 +1,44 @@
 package com.ufps.sgd.domain.service;
 
+import com.ufps.sgd.domain.exception.FileStorageException;
+import com.ufps.sgd.domain.exception.MyFileNotFoundException;
+import com.ufps.sgd.domain.property.FileStorageProperties;
 import com.ufps.sgd.persistence.crud.DocumentoCrudRepository;
 import com.ufps.sgd.persistence.entity.Documento;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
 public class DocumentoService {
 
+    private final Path fileStorageLocation;
+
     @Autowired
     private DocumentoCrudRepository documentoCrudRepository;
+
+    @Autowired
+    public DocumentoService(FileStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+                .toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
 
     @Transactional(readOnly = true)
     public Documento findById(Long id) {
@@ -32,5 +58,37 @@ public class DocumentoService {
     @Transactional
     public void deleteById(Long id) {
         documentoCrudRepository.deleteById(id);
+    }
+
+    public void guardarArchivo(Long id, MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("El nombre del archivo es invalido: " + fileName);
+            }
+            Documento documento = this.findById(id);
+            documento.setNombre(fileName);
+            this.save(documento);
+            fileName = Long.toString(id);
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            throw new FileStorageException("No se puede guardar el archivo " + fileName + ". por favor intente de nuevo", ex);
+        }
+    }
+
+    public Resource cargarArchivo(Long id) {
+        try {
+            Documento documento = this.findById(id);
+            Path filePath = this.fileStorageLocation.resolve(Long.toString(id)).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("Archivo no encontrado");
+            }
+        } catch (MalformedURLException ex) {
+            throw new MyFileNotFoundException("Archivo no encontrado", ex);
+        }
     }
 }
--- src/main/java/com/ufps/sgd/web/controller/DocumentoController.java
+++ src/main/java/com/ufps/sgd/web/controller/DocumentoController.java
@@ -2,18 +2,29 @@ package com.ufps.sgd.web.controller;
 
 import com.ufps.sgd.domain.service.DocumentoService;
 import com.ufps.sgd.persistence.entity.Documento;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/documento")
 public class DocumentoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(DocumentoController.class);
     @Autowired
     private DocumentoService documentoService;
+
 
     @GetMapping("/{id}")
     public Documento buscar(@PathVariable Long id) {
@@ -41,5 +52,29 @@ public class DocumentoController {
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         this.documentoService.deleteById(id);
+    }
+
+    @PostMapping("/{id}/archivo")
+    public void cargarArchivo(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        documentoService.guardarArchivo(id, file);
+    }
+
+    @GetMapping("{id}/descargar")
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long id, HttpServletRequest request) {
+        Resource resource = documentoService.cargarArchivo(id);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        Documento documento = documentoService.findById(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + documento.getNombre() + "\"")
+                .body(resource);
     }
 }
