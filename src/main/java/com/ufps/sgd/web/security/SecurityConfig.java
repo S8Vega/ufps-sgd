4a9626a05827548b495b52955e5280dde851bc73
S8Vega <bsebastianvg18@gmail.com>
2021-05-03 21:56:56 -0500

    agregando roles

--- src/main/java/com/ufps/sgd/web/security/SecurityConfig.java
+++ src/main/java/com/ufps/sgd/web/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ufps.sgd.domain.service.UsuarioService;
 import com.ufps.sgd.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/login").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/usuario").hasRole("ADMINISTRADOR")
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
